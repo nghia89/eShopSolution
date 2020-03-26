3bce8147d3914190fe21f2d69e2562d2e87ba384
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2020-03-26 18:34:43 +0700

    add entity and migration

--- eShopSolution.Data/EF/EShopDbContext.cs
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -1,4 +1,5 @@
-﻿using eShopSolution.Data.Entities;
+﻿using eShopSolution.Data.Configurations;
+using eShopSolution.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,25 +16,25 @@ namespace eShopSolution.Data.EF
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Configure using Fluent API
-            //modelBuilder.ApplyConfiguration(new CartConfiguration());
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
 
-            //modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
-            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
-            //modelBuilder.ApplyConfiguration(new CategoryConfiguration());
-            //modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
-            //modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
 
-            //modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
-            //modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
-            //modelBuilder.ApplyConfiguration(new ContactConfiguration());
-            //modelBuilder.ApplyConfiguration(new LanguageConfiguration());
-            //modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
-            //modelBuilder.ApplyConfiguration(new PromotionConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactConfiguration());
+            modelBuilder.ApplyConfiguration(new LanguageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new PromotionConfiguration());
             //modelBuilder.ApplyConfiguration(new TransactionConfiguration());
 
             //modelBuilder.ApplyConfiguration(new AppUserConfiguration());
             //modelBuilder.ApplyConfiguration(new AppRoleConfiguration());
-            //modelBuilder.ApplyConfiguration(new ProductImageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductImageConfiguration());
 
             //modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims");
             //modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x => new { x.UserId, x.RoleId });
