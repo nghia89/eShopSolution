f6a0a12246cf5ed65e05d0f690ff8671bcdcdd93
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2020-03-28 17:15:38 +0700

    product service

---
+++ eShopSolution.Application/Catalog/Products/PublicProductService.cs
@@ -0,0 +1,25 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Public;
+using eShopSolution.Application.Dtos;
+using eShopSolution.Data.EF;
+using eShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public class PublicProductService : IPublicProductService
+    {
+        private readonly EShopDbContext _context;
+        public PublicProductService(EShopDbContext eShopDbContext)
+        {
+            _context = eShopDbContext;
+        }
+        public Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetProductPagingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- eShopSolution.Data/EF/EShopDbContext.cs
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -2,6 +2,7 @@
 using eShopSolution.Data.Entities;
 using eShopSolution.Data.Extensions;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Text;
 
 namespace eShopSolution.Data.EF
 {
-   public class EShopDbContext: DbContext
+   public class EShopDbContext: IdentityDbContext<AppUser, AppRole, Guid>
     {
         public EShopDbContext(DbContextOptions options) : base(options)
         {
